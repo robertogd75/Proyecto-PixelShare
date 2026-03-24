6a02b3a2b9c3938a1fa935d51ee0ed8ffb5099f8
robertogd75 <robertogarciadelgado2016@gmail.com>
2026-03-24 16:29:17 +0100

    arreglo backend

--- src/main/java/ProyectoAngularJava/example/demo/model/Pixel.java
+++ src/main/java/ProyectoAngularJava/example/demo/model/Pixel.java
@@ -6,17 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "pixels")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Pixel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,4 +28,91 @@ public class Pixel {
 
     @Transient
     private Integer height; // Used in RESIZE messages
+
+    public Pixel() {
+    }
+
+    public Pixel(Long id, int x, int y, String color, Integer size, Long roomId, String type, Integer width, Integer height) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.size = size;
+        this.roomId = roomId;
+        this.type = type;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
 }
--- src/main/java/ProyectoAngularJava/example/demo/model/Room.java
+++ src/main/java/ProyectoAngularJava/example/demo/model/Room.java
@@ -5,17 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "rooms")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +18,64 @@ public class Room {
     private Integer width = 5000;
     private Integer height = 5000;
     private Integer maxParticipants = 100;
+
+    public Room() {
+    }
+
+    public Room(Long id, String code, String name, Integer width, Integer height, Integer maxParticipants) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.maxParticipants = maxParticipants;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(Integer maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
 }
